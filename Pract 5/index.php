132cb520716030097c35c2d61ea1da5b44eb0943
Nicky10110 <nicoleschwegmann@gmail.com>
2016-09-03 10:17:37 +0200

    Changes 5.3

--- Pract 5/index.php
+++ Pract 5/index.php
@@ -44,7 +44,7 @@
 		event.preventDefault(); 
 		$(this).load($(this). attr("href"), completeFunction); 
 	});
-	function completeFunction (){
+	function completeFunction (responseText,textStatus,request){
 		if (textStatus =="error"){
 			$("#content").text("An error occurred during your request : "+ request.status+" "+request.statusText);
 		}
